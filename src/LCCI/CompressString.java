98a3dc0b6e548c27de9ad97505fba0050cccbecf
huhao_MI <1925595006@qq.com>
2020-06-30 15:39:28 +0800

    go fish

---
+++ src/LCCI/CompressString.java
@@ -0,0 +1,38 @@
+package LCCI;
+
+public class CompressString {
+
+    public String compressString(String S) {
+
+        // 朴素算法
+        if(S.length()<2){
+            return S;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 1;
+        char c = S.charAt(0);
+        for(int i=1; i<S.length(); i++){
+            if(S.charAt(i) != c){
+                stringBuilder.append(c);
+                stringBuilder.append(count);
+                c = S.charAt(i);
+                count = 1;
+            }else{
+                count++;
+            }
+
+            if(i == S.length()-1){
+                stringBuilder.append(c);
+                stringBuilder.append(count);
+            }
+        }
+
+        String S2 = stringBuilder.toString();
+
+        if(S.length()>S2.length()){
+            return S2;
+        }else{
+            return S;
+        }
+    }
+}
--- src/LCCI/OneAway.java
+++ src/LCCI/OneAway.java
@@ -1,5 +1,7 @@
 package LCCI;
 
+import java.util.Arrays;
+
 /**
  * @author: huhao
  * @time: 2020/6/29 11:42
@@ -9,12 +11,31 @@ public class OneAway {
 
     public boolean oneEditAway(String first, String second) {
 
+        if(first == null || second == null){
+            return false;
+        }
+
         int length1 = first.length();
         int length2 = second.length();
 
-        if(Math.abs(length1-length2)>1){
-            return false;
+        int i = 0;
+        int j = length1-1;
+        int k = length2-1;
+
+        while(i<length1 && i<length2 && first.charAt(i)==second.charAt(i)){
+            ++i;
         }
+
+        while (j>=0 && k>=0 && first.charAt(j) == second.charAt(k)){
+            --j;
+            --k;
+        }
+        return j-i<1 && k-i<1;
+    }
+
+    public static void main(String[] args) {
+        OneAway oneAway = new OneAway();
+        oneAway.oneEditAway("teacher", "treacher");
 
     }
 }
