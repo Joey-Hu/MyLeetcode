ac9fed9df32fd661e9c5cb04f993de0deb4d6ac4
huhao <1925595006@qq.com>
2020-07-01 22:53:14 +0800

    go fish

--- src/LCCI/KthNodeFromEndOfList.java
+++ src/LCCI/KthNodeFromEndOfList.java
@@ -22,7 +22,24 @@ public class KthNodeFromEndOfList {
             value = stack.pop();
         }
         return value;
+    }
 
+    public int kthToLast2(ListNode head, int k) {
+        // 双指针
+        ListNode p = head;
+        ListNode q = head;
+
+        // 先让q向后移动k，然后p,q一起向后移动，当q移动到null时，p指向的结点就是倒数第k个节点
+        for(int i=0; i<k; i++){
+            q = q.next;
+        }
+
+        while (q != null){
+            p = p.next;
+            q = q.next;
+        }
+
+        return p.val;
     }
 
     private class ListNode {
