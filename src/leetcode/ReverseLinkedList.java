afdc942da7e7796dd213a98a53ef9462730fe1bb
huhao <1925595006@qq.com>
2020-09-01 22:40:00 +0800

    “链表

---
+++ src/leetcode/ReverseLinkedList.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+/**
+ * @author: huhao
+ * @time: 2020/9/1 22:22
+ * @desc: https://leetcode.com/problems/reverse-linked-list/
+ */
+public class ReverseLinkedList {
+    /**
+     * 递归
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
