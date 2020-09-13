4d55fa5fa991e326f25d0ee63a107bbd28b27d03
huhao_MI <1925595006@qq.com>
2020-09-14 07:23:41 +0800

    go newbie

--- src/leetcode/backtrack/NQueens.java
+++ src/leetcode/backtrack/NQueens.java
@@ -1,5 +1,7 @@
 package leetcode.backtrack;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,7 +11,56 @@ import java.util.List;
  */
 public class NQueens {
 
+    private List<List<String>> res;
+
     public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        res = new ArrayList<>();
+        dfs(board, 0, res);
+        return res;
 
     }
+
+    private void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if (colIndex == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for(int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+
+    private boolean validate(char[][] board, int x, int y) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < y; j++) {
+                if (board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    
 }
