d4c7c250537b9692b1a7b780fa54e9326a7cd097
Joey-Hu <1925595006@qq.com>
2020-10-15 18:21:34 +0800

    tree

---
+++ src/leetcode/tree/M538_ConvertBSTtoGreaterTree.java
@@ -0,0 +1,73 @@
+package leetcode.tree;
+
+import com.sun.org.apache.xml.internal.security.utils.HelperNodeList;
+
+import java.util.*;
+
+/**
+ * @author: huhao
+ * @time: 2020/10/15 16:07
+ * @desc: https://leetcode.com/problems/convert-bst-to-greater-tree/
+ */
+public class M538_ConvertBSTtoGreaterTree {
+
+    /**
+     * reverse inorder traverse
+     * @param root
+     * @return
+     */
+    public TreeNode convertBST(TreeNode root) {
+        int sum = 0;
+        helper(root, sum);
+        return root;
+    }
+
+    private int helper(TreeNode root, int sum) {
+        if (root == null) {
+            return sum;
+        }
+        int right = helper(root.right, sum);
+        root.val += right;
+        int left = helper(root.left, root.val);
+        return left;
+    }
+
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            inorder(root.left, list);
+        }
+        list.add(root.val);
+        if (root.right != null) {
+            inorder(root.right, list);
+        }
+    }
+
+    private int sum(List<Integer> list, int start) {
+        int sum = 0;
+        for (int i = start; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        return sum;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {}
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
