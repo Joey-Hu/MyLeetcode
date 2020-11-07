8d8a2aad5b39b485f34c2942d551168347b3e5f5
Joey-Hu <1925595006@qq.com>
2020-11-07 08:59:14 +0800

    modified Test

--- src/leetcode/Test.java
+++ src/leetcode/Test.java
@@ -12,42 +12,11 @@ import java.util.List;
  */
 public class Test {
 
-    public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[]{-1, -1};
-        int low = 0;
-        int high = nums.length - 1;
-
-        if (nums.length == 0) {
-            return res;
-        }
-
-        // 左边界
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] >= target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        res[0] = low;
-
-        // 右边界
-        high = nums.length - 1;
-        while(low < high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] <= target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        res[1] = high;
-        return res;
-    }
-
     public static void main(String[] args) {
-        int[] nums = new int[]{5,7,7,8,8,10};
-        new Test().searchRange(nums, 8);
+        String str = "  sky   is blue.";
+        String[] split = str.split(" ");
+        for (String s : split) {
+            System.out.println(s);
+        }
     }
 }
