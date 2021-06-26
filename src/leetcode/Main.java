335406fafbf7c3ae9ac00f6f2acb261cf3657d78
Joey-Hu <1925595006@qq.com>
2021-06-26 16:20:32 +0800

    codeTop challenge

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -13,25 +13,41 @@ import java.util.Stack;
  */
 import java.util.*;
 public class Main {
+
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+        int maxLen = 0;
+        String res= "";
+
+        for (int i = 0; i < s.length()-1; i ++) {
+            int len1 = helper(s, i, i+1);
+            int len2 = helper(s, i, i);
+            int len = Math.max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (maxLen-1) / 2;
+                end = i + maxLen / 2;
+                
+                res = s.substring(start, end);
+            }
+        }
+        return res;
+    }
+
+    private int helper(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left --;
+            right ++;
+        }
+        return right - left - 1;
+    }
+
     public static void main(String[] args) {
-        for (int i = 1; i < 1000; i ++) {
-            if (isValid(i)) {
-                System.out.println(i);
-            }
-
-        }
+        System.out.println(new Main().longestPalindrome("babad"));
     }
 
-    private static boolean isValid(int num) {
-        while (num > 0) {
-            int med = num % 10;
-            if (med == 5) {
-                return false;
-            }
-            num = num / 10;
-        }
-        return true;
-    }
+
 }
 
 
